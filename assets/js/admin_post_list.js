a5de4d2e1bf418af0d0a535ea11e4c090ea62565
adev <adev2000@gmail.com>
2017-07-31 17:33:06 +0200

    Rendu via html directly

--- assets/js/admin_post_list.js
+++ assets/js/admin_post_list.js
@@ -4,7 +4,26 @@ import axios from 'axios'
 Vue.prototype.$http = axios;
 
 Vue.component('data-table', {
-  template: '<table></table>',
+  template: `
+  <table>       
+  <thead>
+      <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Author</th>
+          <th><i class="fa fa-calendar" aria-hidden="true"></i>Publish at</th>
+      </tr>
+  </thead>            
+  <tbody>
+    <template v-for="post in posts">
+        <tr>{{post.name}}</tr>
+        <tr>{{post.title}}</tr>
+        <tr>{{post.author.full_name}}</tr>
+        <tr>{{post.publish_at}}</tr>
+    </template>
+  </tbody>
+  </table>
+  `,
   props: ['posts'],
   data() {
     return {
@@ -22,25 +41,25 @@ Vue.component('data-table', {
     posts(val, oldVal) {
       let vm = this;
       vm.rows = [];
-      // You should _probably_ check that this is changed data... but we'll skip that for this example.
-      val.forEach(function (item) {
-        // Fish out the specific column data for each item in your data set and push it to the appropriate place.
-        // Basically we're just building a multi-dimensional array here. If the data is _already_ in the right format you could
-        // skip this loop...
-        let row = [];
-
-        row.push(item.id);
-        row.push(item.title);
-        row.push(item.author.full_name);
-        row.push(item.published_at);
-
-        vm.rows.push(row);
-      });
+      // // You should _probably_ check that this is changed data... but we'll skip that for this example.
+      // val.forEach(function (item) {
+      //   // Fish out the specific column data for each item in your data set and push it to the appropriate place.
+      //   // Basically we're just building a multi-dimensional array here. If the data is _already_ in the right format you could
+      //   // skip this loop...
+      //   let row = [];
+      //
+      //   row.push(item.id);
+      //   row.push(item.title);
+      //   row.push(item.author.full_name);
+      //   row.push(item.published_at);
+      //
+      //   vm.rows.push(row);
+      // });
 
       // Here's the magic to keeping the DataTable in sync.
       // It must be cleared, new rows added, then redrawn!
       vm.dtHandle.clear();
-      vm.dtHandle.rows.add(vm.rows);
+      //vm.dtHandle.rows.add(vm.rows);
       vm.dtHandle.draw();
     }
   },
@@ -59,6 +78,7 @@ Vue.component('data-table', {
 });
 
 new Vue({
+  delimiters: ['${', '}'],
   el: '#tabledemo',
   data: {
     posts: [],
@@ -79,8 +99,17 @@ new Vue({
 
     this.$http.get('/en/admin/post/list')
       .then(response => {
-        vm.posts = response.data
+        vm.posts = response.data;
       });
+  },
+  updated() {
+    let vm = this;
+
+    $(vm.$el).DataTable({
+      searching: true,
+      paging: false,
+      info: false
+    });
   },
   methods: {
     ajouter() {
@@ -95,13 +124,5 @@ new Vue({
           published_at: '',
       });
     }
-  }
-});
-
-var apptest = new Vue({
-  delimiters: ['${', '}'],
-  el: '#apptest',
-  data: {
-    message: 'Hello Vue!'
   }
 });
